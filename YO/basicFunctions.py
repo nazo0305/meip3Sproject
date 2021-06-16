dfb5fd6b94bfb7f846723530b414fa4302336a09
yuta ono <youcana40627@gmail.com>
2021-06-16 14:58:25 +0900

    UDP通信

--- YO/basicFunctions.py
+++ YO/basicFunctions.py
@@ -1,4 +1,5 @@
 import cv2
+import socket
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -25,10 +26,24 @@ def drawSmallCircles(img, circles, maxR):
     '''
     for circle in circles[0, :]:
         # 円が検出されなかった場合[0 0 0]が渡されるのでそれは省きたい
-        print(circle)
         if 0 < circle[2] <= maxR:
             # 円周を描く
             cv2.circle(img, (circle[0], circle[1]), circle[2], (0, 255, 0), 2)
             # 円の中心を描く
             cv2.circle(img, (circle[0], circle[1]), 2, (0, 0, 255), 3)
     return img
+
+
+def sendInfoByUDP(item):
+    '''
+    （仮）
+    itemをUDP通信で送信する
+    参考:https://jump1268.hatenablog.com/entry/2018/11/25/143459
+    '''
+
+    HOST = '127.0.0.1'
+    PORT = '50007'
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sendItem = str(item)
+    client.sendto(sendItem.encode('utf-8'), (HOST, PORT))
