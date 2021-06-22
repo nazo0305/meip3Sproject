bce591fefe9bebe59ab8b397268372ee843eed27
jell <euler31415@icloud.com>
2021-06-22 17:21:13 +0900

    test

---
+++ openCV/test/subtract.py
@@ -0,0 +1,30 @@
+# 背景差分のテスト
+
+import cv2
+
+
+def main():
+    cap = cv2.VideoCapture(0)
+    subtractor = cv2.createBackgroundSubtractorMOG2()
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+
+    while 1:
+        ret, frame = cap.read()
+        cv2.imshow("frame", frame)
+
+        diff = subtractor.apply(frame)
+        cv2.imshow('diff', diff)
+
+        # モーフォロジカル処理
+        diff_morp = cv2.morphologyEx(diff, cv2.MORPH_OPEN, kernel)
+        cv2.imshow('diff_morp', diff_morp)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
