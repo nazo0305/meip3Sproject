d2f671affbb7cf258bb410fe697c03e082b78a79
Nazo0305 <nazo.masa0305@gmail.com>
2021-06-23 13:27:37 +0900

    Scriptの移植とPhotonの導入

---
+++ TeleShooterAlpha/Assets/_Myassets/Personal/mk/Scripts/ObjectDelete.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDelete : MonoBehaviour
+{
+     float timer = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject, timer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
