dcf8a6d0dac4fc120631f4d034108f85c786206d
jell <euler31415@icloud.com>
2021-06-29 17:27:41 +0900

    locate

---
+++ OpenCV/src/locate.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy
+
+
+def circle(image, lower, upper):
+    """
+    @param image BGR / HSV color space.
+    @param lower Lowerbounds for BGR / HSV.
+    @param upper Upperbounds for BGR / HSV.
+    @return (Center, Radius): tuple. All values are of type float.
+    """
+
+    binary = cv2.inRange(image, numpy.array(lower), numpy.array(upper))
+    contours, hierarchy = cv2.findContours(binary, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    # cv2.drawContours(image, contours, -1, (0, 0, 255), 3)
+
+    if not contours:
+        return None
+
+    cont = max(contours, key=cv2.contourArea)
+    (x, y), (h, w), theta = cv2.fitEllipse(cont)
+    return ((x, y), min(h, w))
---
+++ OpenCV/test/locate.test.py
@@ -0,0 +1,28 @@
+import sys
+import cv2
+
+sys.path.append('../src/')
+
+import locate
+
+
+def main():
+    cap = cv2.VideoCapture(0)
+
+    while 1:
+        ret, frame = cap.read()
+        cv2.imshow("frame", frame)
+
+        (x, y), r = locate.circle(frame, [200, 200, 200], [255, 255, 255])
+        located = cv2.circle(frame, (int(x), int(y)), int(r), (0, 255, 0))
+        cv2.imshow("located", located)
+
+        if cv2.waitKey(30) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
